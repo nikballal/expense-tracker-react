788bd3c90944721bcb290b5e3c96b3849d5c9101
nikballal <nikhil.ballal1994@gmail.com>
2022-06-11 13:20:23 +1000

    UI components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenseBox from "./components/IncomeExpenseBox";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenseBox />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
 }
 
 export default App;
